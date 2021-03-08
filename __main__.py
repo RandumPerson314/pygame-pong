dce8a41bc8613b3514925d2057cacb0ad93fb3a2
RandumPerson314 <46033255+RandumPerson314@users.noreply.github.com>
2021-03-07 19:38:24 -0500

    copy pasta from other file

--- __main__.py
+++ __main__.py
@@ -1,1 +1,17 @@
+import sys, pygame
+pygame.init() # pylint: disable=no-member
+from win32api import GetSystemMetrics # pylint: disable=no-name-in-module
 
+print("Width =", GetSystemMetrics(0))
+print("Height =", GetSystemMetrics(1))
+
+
+size = width, height = 1920, 1080
+
+screen = pygame.display.set_mode(size)
+
+while 1: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit() # pylint: disable=no-member
+
+    pygame.display.flip()
